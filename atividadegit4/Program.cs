9e6ab43e9849bc0ca5b395c4e37000b97abee852
luazrk <luazrk@gmail.com>
2019-10-21 21:55:56 -0300

    primeiro commit adicionado

---
+++ atividadegit4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividadegit4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+        public static int somar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+    }
+}
